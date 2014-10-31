9d15f08a4411d77712a3ff7afaa51f825072f1da
Madhu Venugopal <madhu@socketplane.io>
2014-10-31 16:15:41 -0700

    fixed build.sh with the new naming

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc -I../ -I../include -I../third-party/ovs/include -I../third-party/ovs -o sp-ovs-driver sp-ovs-driver.c -L../lib -lopenvswitch -lofproto  -lrt -lm -pthread -w
+gcc -I../ -I../include -I../third-party/ovs/include -I../third-party/ovs -o ovs-driver ovs-driver.c -L../lib -lopenvswitch -lofproto  -lrt -lm -pthread -w
